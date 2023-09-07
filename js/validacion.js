5af60da2e96484c7d091df18cc73d8364a3e1bf8
DiegoDev <127701420+Diego-Gil@users.noreply.github.com>
2023-09-07 16:23:37 -0500

    Add files via upload

---
+++ js/validacion.js
@@ -0,0 +1,25 @@
+//Haz tú validación en javascript acá
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
+    }
+  
+    if (input.validity.valid) {
+      input.parentElement.classList.remove("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+      input.parentElement.classList.add("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML =
+        mostrarMensajeDeError(tipoDeInput, input);
+    }
+  }
+
+  const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+  ];
+
+  
